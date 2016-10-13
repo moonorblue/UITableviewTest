8607e17a925c7fc544d3277e37b41a5f6e002591
KaeJer Cho <kaejercho@router.asus.com>
2016-10-14 00:41:49 +0800

    first commit

--- tbview/ViewController.m
+++ tbview/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    NSMutableArray *array;
+    NSMutableArray *hide;
+    NSInteger currentHide;
+}
 
 @end
 
@@ -16,6 +20,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    array = [[NSMutableArray alloc]init];
+    hide = [[NSMutableArray alloc]init];
+    for (int x =0; x<20; x++) {
+        NSString* str = [NSString stringWithFormat:@"%d",x];
+        [array addObject:str];
+        [hide addObject:@NO];
+    }
+    _tb.dataSource = self;
+    _tb.delegate = self;
+
+    [_tb reloadData];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -25,5 +40,65 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 20;
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if (![[hide objectAtIndex:section]  isEqual: @YES]) {
+        return 5;
+    }
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell;
+    cell = [_tb dequeueReusableCellWithIdentifier:@"cell"];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    }
+    NSInteger section = indexPath.section;
+    cell.textLabel.text = [array objectAtIndex:section];
+    return cell;
+}
+
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    UIView *header = [[UIView alloc]initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 20)];
+    UITapGestureRecognizer *singleTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(gestureHandler:)];
+    singleTapRecognizer.numberOfTouchesRequired = 1;
+    singleTapRecognizer.numberOfTapsRequired = 1;
+    [header addGestureRecognizer:singleTapRecognizer];
+    UIView *tagview = [singleTapRecognizer view];
+    tagview.tag = section;
+    header.backgroundColor = [UIColor redColor];
+    return header;
+}
+- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section {
+    UIView *footer =[[UIView alloc]initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 20)];
+    footer.backgroundColor = [UIColor blueColor];
+    return footer;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+{
+    return 100;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 80;
+}
+-(void)gestureHandler:(UIGestureRecognizer *)gestureRecognizer {
+    NSInteger section = gestureRecognizer.view.tag;
+    if ([[hide objectAtIndex:section]  isEqual: @YES]) {
+        [hide replaceObjectAtIndex:section withObject:@NO];
+    }
+    else {
+        [hide replaceObjectAtIndex:section withObject:@YES];
+    }
+    
+    [_tb reloadSections:[NSIndexSet indexSetWithIndex:section] withRowAnimation:UITableViewRowAnimationAutomatic];
+    
+}
 
 @end
